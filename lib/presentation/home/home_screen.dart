3e94b62342024f4b59d9ca6ab42324ba6b29b369
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2024-08-30 16:51:06 +0600

    Assaignment Done

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ostad_assaignment_3/presentation/widget/responsive_builder.dart';
 import 'widget_class.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,88 +7,181 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Align(
-          alignment: Alignment.centerRight,
-          child: Padding(
-            padding: EdgeInsets.only(right: 20),
-
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('HUMMING'),
-                Text('BIRD.'),
-              ],
+    return ResponsiveBuilder(
+      mobile: Scaffold(
+        appBar: AppBar(
+          title: const Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('HUMMING'),
+                  Text('BIRD.'),
+                ],
+              ),
             ),
           ),
         ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              _buildDrawerHeader(),
+              const SizedBox(height: 20),
+              DrawerListTile(
+                icon: const Icon(Icons.smart_display_outlined),
+                textName: 'Episode',
+              ),
+              DrawerListTile(
+                icon: const Icon(Icons.feedback_sharp),
+                textName: 'About',
+              ),
+            ],
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'FLUTTER WEB.',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+              ),
+              const Text(
+                'THE BASICS',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+              ),
+              const SizedBox(height: 20),
+              const Wrap(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(24.0),
+                    child: Text(
+                      'In this course we will go over the basics of using Flutter web for development. Topics will include Responsive Layout, Deploying, Font Changes, Hover functionality, Modals, and more.',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(3),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'join course',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            _buildDrawerHeader(),
-            const SizedBox(
-              height: 20,
+      desktop: Scaffold(
+        appBar: AppBar(
+          title: const Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Text('HUMMING \nBIRD.'),
             ),
-            DrawerListTile(
-              icon: const Icon(Icons.smart_display_outlined),
-              textName: 'Episode',
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Episodes',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
             ),
-            DrawerListTile(
-              icon: const Icon(Icons.feedback_sharp),
-              textName: 'About',
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'About',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              'FLUTTER WEB.',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
-            ),
-            const Text(
-              'THE BAICS',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Wrap(
+        body: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Center(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                const Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'FLUTTER WEB.',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      Text(
+                        'THE BASICS',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'In this course we will go over the basics of using Flutter Web for development. Topics will include Responsive Layout, Deploying, Font Changes, Hover functionality, Modals, and more.',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
                 Padding(
-                  padding: EdgeInsets.all(24.0),
-                  child: Text(
-                    'In this course we will go over the basics of using Flutter web for development. Topics will include Responsive Layout , Deploying, Font Changes, Hover functionality, Modals and more.',
-                    style: TextStyle(
-                      fontSize: 18,
+                  padding: const EdgeInsets.only(
+                    right: 150
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        backgroundColor: Colors.green,
+
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Join course',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
                     ),
                   ),
-                )
+                ),
               ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                shape: BeveledRectangleBorder(
-                  borderRadius: BorderRadius.circular(3),
-                ),
-              ) ,
-              onPressed: () {},
-              child: const Text('join course', style: TextStyle(
-                fontSize: 14,
-                color: Colors.white,
-                fontWeight: FontWeight.w500
-              ),),
-            ),
-          ],
+          ),
         ),
       ),
     );
@@ -95,27 +189,29 @@ class HomeScreen extends StatelessWidget {
 
   Widget _buildDrawerHeader() {
     return const DrawerHeader(
-        decoration: BoxDecoration(color: Colors.green),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'SKILL UP NOW',
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w900),
+      decoration: BoxDecoration(color: Colors.green),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'SKILL UP NOW',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
               ),
-              Text(
-                'TAP HERE',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.white,
-                ),
+            ),
+            Text(
+              'TAP HERE',
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white,
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
